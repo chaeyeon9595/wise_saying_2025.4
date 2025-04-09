7a06a212481bb49bcabd5071fcb96fff039c9c8f
cjang5189@gmail.com <cjang5189@gmail.com>
2025-04-09 20:34:57 +0900

    2단계, 명언 입력받기

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -15,11 +15,24 @@ public class App {
     public void run(){
         System.out.println("== motivation 앱 실행 == ");
 
-        System.out.print("명령어) ");
-        String cmd = sc.nextLine().trim(); //앱이 가진 sc를 사용. trim -> 혹시 모를 좌우 공백 제거
+        while(true){
 
-        if(cmd.equals("exit")){ //equals로 내용비교. 그냥 == 으로 비교하면 주소값을 비교하는 것!!
-            System.out.println("== motivation 앱 종료 ==");
+
+            System.out.print("명령어) ");
+            String cmd = sc.nextLine().trim(); //앱이 가진 sc를 사용. trim -> 혹시 모를 좌우 공백 제거
+
+            if(cmd.equals("add")){
+                System.out.print("명언 : ");
+                String content = sc.nextLine();
+                System.out.print("인물 : ");
+                String person = sc.nextLine();
+                System.out.println("1번 명언이 등록되었습니다.");
+            }
+
+            if(cmd.equals("exit")){ //equals로 내용비교. 그냥 == 으로 비교하면 주소값을 비교하는 것!!
+                System.out.println("== motivation 앱 종료 ==");
+                break;
+            }
         }
 
     }
