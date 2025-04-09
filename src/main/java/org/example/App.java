8971c7eda26a4ef5168fdc91d11616693eea2969
cjang5189@gmail.com <cjang5189@gmail.com>
2025-04-09 20:40:15 +0900

    3단계, 명언 입력받기

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -15,6 +15,8 @@ public class App {
     public void run(){
         System.out.println("== motivation 앱 실행 == ");
 
+        int lastId = 1;
+
         while(true){
 
 
@@ -26,7 +28,8 @@ public class App {
                 String content = sc.nextLine();
                 System.out.print("인물 : ");
                 String person = sc.nextLine();
-                System.out.println("1번 명언이 등록되었습니다.");
+                System.out.printf("%d번 명언이 등록되었습니다.\n", lastId);
+                lastId++;
             }
 
             if(cmd.equals("exit")){ //equals로 내용비교. 그냥 == 으로 비교하면 주소값을 비교하는 것!!
