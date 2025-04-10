2f72abd1fc8f0149ca780200e96f47f604aa3d74
cjang5189@gmail.com <cjang5189@gmail.com>
2025-04-10 20:44:34 +0900

    Container를 도입하여 구조 개선

--- src/main/java/org/project/Container.java
+++ src/main/java/org/project/Container.java
@@ -4,8 +4,19 @@ import java.util.Scanner;
 
 public class Container {
     private static Scanner sc;
-    public static void init(){
+
+    // 공통적으로 사용하는 자원들을 모아두는 공간 초기화
+    public static void init() {
         sc = new Scanner(System.in);
+    }
+
+    // 공통적으로 사용하는 자원들을 모아두는 공간 해제
+    public static void close() {
+        sc.close();
+    }
+
+    public static Scanner getScanner() {
+        return sc;
     }
 
 }
